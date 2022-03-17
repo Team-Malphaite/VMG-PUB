86a1f53d68adc0ee16e0d1a553a423eb189b9b1d
tkdans7589 <32192086+tkdans7589@users.noreply.github.com>
2022-03-18 01:41:58 +0900

    feat : make network manager

---
+++ VMG-PUB/Assets/Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public string NickName = "test1";
+    public string gameVersion = "1.0";
+    
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        OnLogin();
+    }
+
+    void OnLogin()
+    {
+        PhotonNetwork.GameVersion = this.gameVersion;
+        PhotonNetwork.NickName = this.NickName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        // base.OnConnectedToMaster();
+        Debug.Log("Connected !!!");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        // base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log("Failed join room!!!");
+        this.CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        // base.OnJoinedRoom();
+        Debug.Log("Joined Room");
+        PhotonNetwork.Instantiate("Prefabs/Character/TestCharacter", new Vector3(0,0,0), Quaternion.identity);
+    }
+
+    void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4});
+    }
+}
