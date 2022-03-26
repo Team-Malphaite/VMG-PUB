fcdbe377c8cf6ebc80b3c12b59cafd11d72d678e
tkdans7589 <32192086+tkdans7589@users.noreply.github.com>
2022-03-26 22:29:32 +0900

    feat : gate and bridge anim and pass

---
+++ VMG-PUB/Assets/Scripts/Controllers/GateController10.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateController10 : MonoBehaviour
+{
+    GameObject go;
+    Animator anim;
+    MeshCollider mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        go = GameObject.Find("Gate10");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnTriggerEnter(Collider col) {
+        anim = go.GetComponent<Animator>();
+        mesh = go.GetComponent<MeshCollider>();
+        if(col.gameObject.tag == "Player") {
+            anim.SetBool("open", true);
+            mesh.convex = false;
+        }
+    }
+
+    void OnTriggerExit(Collider col) {
+        mesh = go.GetComponent<MeshCollider>();
+        anim = go.GetComponent<Animator>();
+        anim.SetBool("open", false);
+        mesh.convex = true;
+
+    }
+}
