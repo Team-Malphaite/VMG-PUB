386315f831bb4df97267725fc86b3fb54d800bcf
dlrkdmsdkdld <78574719+dlrkdmsdkdld@users.noreply.github.com>
2022-04-05 01:43:30 +0900

    FEAT : add firebase module
    로그인하면 이메일 정보 받아오고
    파이어스토리지 데이터 읽기 쓰기 모듈 추가
    
    Update UI_Login.cs

--- VMG-PUB/Assets/Scripts/Managers/FirebaseWebGL/Scripts/AuthHandler.cs
+++ VMG-PUB/Assets/Scripts/Managers/FirebaseWebGL/Scripts/AuthHandler.cs
@@ -8,6 +8,8 @@ using UnityEngine.UI;
        // public InputField passwordInput;
  
         public string statusText;
+        public  string emailAddress; // 로그인한 사용자의 이메일 정보를 담고있는 변수
+        public  string playerDataBuffer;// 데이터를 읽어오면 여기에 저장된다
  
         private void Start()
         {
@@ -28,6 +30,15 @@ using UnityEngine.UI;
         {
             statusText = Infotext;
         }
+        private void GetEmailData(string Infotext)
+        {
+                emailAddress=Infotext;
+        }
+        private void ReadPlayerData(string Infotext)
+        {
+                playerDataBuffer=Infotext;
+        }
+
          //public static AuthHandler Instance; // singleton 변수
 
  
@@ -35,7 +46,14 @@ using UnityEngine.UI;
         public void SignInWithGoogle() =>
             FirebaseAuth.SignInWithGoogle(gameObject.name, "DisPlayInfo", "DisplayError");
  
- 
+         public void GetUserAuthDataEmail() =>//사용자의 이메일 정보 받아옴 이를 토대로 데이터 베이스 생성시킨다.
+            FirebaseAuth.GetUserAuthDataEmail(gameObject.name, "GetEmailData", "DisplayError");
+         public void SetDocument() =>  //사용자가 처음 로그인할때 사용자 정보 저장(문서 작성 - 컬렉션도 없을때 ) 
+            FirebaseAuth.SetDocument("user",  emailAddress,  emailAddress,"0", gameObject.name,"DisplayInfo", "DisplayError");
+         public void GetDocument() =>   //데베 읽기
+            FirebaseAuth.GetDocument("user", emailAddress, gameObject.name, "ReadPlayerData",  "DisplayError");
+
+
  
        // public void CreateUserWithEmailAndPassword() =>
            ///d FirebaseAuth.CreateUserWithEmailAndPassword(emailInput.text, passwordInput.text, gameObject.name, "DisPlayInfo", "DisplayError");
