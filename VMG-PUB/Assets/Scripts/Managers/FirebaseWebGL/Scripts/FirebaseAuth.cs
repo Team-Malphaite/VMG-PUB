6638e08ce620bad4e753397fc85eba617c3e91b4
dlrkdmsdkdld <78574719+dlrkdmsdkdld@users.noreply.github.com>
2022-04-05 01:43:30 +0900

    FEAT : add firebase module
    로그인하면 이메일 정보 받아오고
    파이어스토리지 데이터 읽기 쓰기 모듈 추가

--- VMG-PUB/Assets/Scripts/Managers/FirebaseWebGL/Scripts/FirebaseAuth.cs
+++ VMG-PUB/Assets/Scripts/Managers/FirebaseWebGL/Scripts/FirebaseAuth.cs
@@ -3,11 +3,19 @@ using System.Runtime.InteropServices;
 
     public static class FirebaseAuth
     {
-     
-
+        //////////////파이어 Auth///////////////////////
 
         [DllImport("__Internal")]
         public static extern void SignInWithGoogle(string objectName, string callback, string fallback);
+
+        [DllImport("__Internal")]
+        public static extern void GetUserAuthDataEmail(string objectName, string callback, string fallback);
+         //////////////파이어 스토어 ///////////////////////
+        [DllImport("__Internal")]
+        public static extern void SetDocument(string collectionPath, string documentId, string oneValue,string towValue, string objectName,string callback,string fallback);
+
+        [DllImport("__Internal")]
+        public static extern void GetDocument(string collectionPath, string documentId, string objectName, string callback, string fallback);
 
 
 
